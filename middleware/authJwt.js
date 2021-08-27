d56235eefc61d4fda22635b48d22433425ca5478
wilfried <wilfried.odorico@gmail.com>
2021-08-27 10:32:08 +0200

    modification du middleware authJwt

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -1,29 +1,25 @@
 const JWT = require("jsonwebtoken");
 
 let validateToken = (req, res, next) => {
-  let tokenValue = req.headers["authorisation"];
+  let token = req.headers["authorization"];
 
-  if (tokenValue) {
-    JWT.verify(tokenValue, process.env.TOKEN_SECRET, (error, data) => {
-      if (error) {
-        return res.status(500).json({
-          status: 0,
-          message: "Le token n'est pas valide",
-        });
-      } else {
-        req.data = data;
-        next();
-      }
-    });
-  } else {
-    // pas de token dans le headers
-    return res.status(404).json({
-      status: 0,
-      message: "Token demandé",
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!",
     });
   }
+
+  JWT.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({
+        message: "Unauthorized",
+      });
+    }
+    req.data = decoded;
+    next();
+  });
 };
 
 module.exports = {
-  checkToken: validateToken
+  checkToken: validateToken,
 };
