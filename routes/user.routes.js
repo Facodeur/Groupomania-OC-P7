afe12c40a3335692faf851a57f3de4b183f8b54a
wilfried <wilfried.odorico@gmail.com>
2021-08-27 10:31:35 +0200

    creation route profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
+const authJwt = require("../middleware/authJwt");
 
 const router = express.Router();
 
-// auth
+// authentification
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
+
+// Données utilisateur
+router.post("/profile", authJwt.checkToken, userController.userInfo);
+
 
 module.exports = router;
