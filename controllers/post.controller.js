fbedc73de5e00e966aaf8aeab6a106ef5139bf6a
wilfried <wilfried.odorico@gmail.com>
2021-09-01 10:03:18 +0200

    ajout route update + creation du controller et modification du getallposts lister en ordre DESC

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,52 +2,78 @@ const postModel = require("../models").Post;
 const userModel = require("../models").User;
 
 exports.getAllPosts = (req, res) => {
-
   postModel
-    .findAll({ order: [ ["createdAt", "DESC"]]})
-    .then(posts => {
-      res.status(200).json({
-        status: 1,
-        data: posts
-      })
+    .findAll({
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          model: userModel,
+          attributes: ["username"],
+        },
+      ],
     })
-    .ctach(err => {
-      res.status(500).json({
-        error: err.message
-      })
+    .then((posts) => {
+      res.status(200).json({ status: 1, data: posts });
     })
-
-}
+    .catch((err) => { 
+      res.status(500).json({ message: err.message });
+    });
+};
 
 exports.createPost = (req, res) => {
   const user_id = req.data.id;
 
   userModel
-    .findOne({
-      where: {
-        id: user_id,
-      },
-    })
+    .findOne({ where: { id: user_id } })
     .then((user) => {
+      if (req.body.content === "" || req.body.content === undefined) {
+        return res.status(200).json({ message: "Veuillez écrire quelque chose" });
+      }
       if (user) {
         postModel
           .create({
             content: req.body.content,
             picture: req.file !== undefined ? `./images/${req.file.filename}` : "",
-            userId: user.id,
+            userId: user.id
           })
           .then(() => {
-            res.status(201).json({
-              status: 1,
-              message: "Post enregistré",
-            });
+            res.status(201).json({ status: 1, message: "Post enregistré" });
           })
           .catch((err) => {
-            res.status(500).json({ status: 0, error: err.message });
+            res.status(500).json({ message: err.message });
           });
       }
     })
     .catch((err) => {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({ message: err.message });
+    });
+};
+
+exports.updatePost = (req, res) => {
+  const user_id = req.data.id;
+
+  postModel
+    .findOne({ where: { id: req.params.id } })
+    .then((post) => {
+      if (post.userId === user_id) {
+        postModel
+          .update(
+            {
+              content: req.body.content,
+              picture: post.picture,
+            },
+            { where: { id: post_id } }
+          )
+          .then((post) => {
+            if (post == 1) {
+              res.send({ message: "Le post a été modifié" });
+            }
+          });
+      } else {
+        res.status(401).json({ status: 0, message: "Vous n'êtes pas autorisé" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Erreur lors de la mise à jour" });
     });
 };
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,10 +3,10 @@ const postController = require("../controllers/post.controller");
 const authJwt = require("../middleware/authJwt");
 const multer = require("../middleware/multer-config");
 
-
 const router = express.Router();
 
-router.get('/', authJwt, postController.getAllPosts);
+router.get("/", authJwt, postController.getAllPosts);
 router.post("/", authJwt, multer, postController.createPost);
+router.put("/:id", authJwt, multer, postController.updatePost);
 
 module.exports = router;
