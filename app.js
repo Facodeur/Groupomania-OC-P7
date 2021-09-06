0c19c3d118f03686b3fc15429a7c9fde83163994
wilfried <wilfried.odorico@gmail.com>
2021-09-06 15:37:47 +0200

    installation de helmet + activation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
+const helmet = require("helmet");
 const PORT = process.env.PORT;
 const path = require('path');
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
 
 const app = express();
+
+app.use(helmet());
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
