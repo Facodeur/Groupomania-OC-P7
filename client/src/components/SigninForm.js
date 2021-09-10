83ccb678ec668374da14c4d17d1f34dae7847217
wilfried <wilfried.odorico@gmail.com>
2021-09-10 08:53:41 +0200

    creation des components signin et signup form

---
+++ client/src/components/SigninForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SigninForm = () => {
+  return (
+    <div>
+      Se connecter
+    </div>
+  )
+}
+
+export default SigninForm
---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignupForm = () => {
+  return (
+    <div>
+      S'inscrire
+    </div>
+  )
+}
+
+export default SignupForm
