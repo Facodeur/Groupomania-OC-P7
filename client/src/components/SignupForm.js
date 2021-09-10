232c3a917999259f71475c0f71a5ea0359375369
wilfried <wilfried.odorico@gmail.com>
2021-09-10 09:45:46 +0200

    ajout du formulaire d'inscription

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const SignupForm = () => {
   return (
-    <div>
-      S'inscrire
+    <div className="container">
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate" />
+              <label for="email">Nom utilisateur</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate" />
+              <label for="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="password" type="password" className="validate" />
+              <label for="password">Mot de passe</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="confirm-password"
+                type="password"
+                className="validate"
+              />
+              <label for="confirm-password">Confirmation du mot de passe</label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignupForm
+export default SignupForm;
