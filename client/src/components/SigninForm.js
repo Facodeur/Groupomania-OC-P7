42f3b9bbc0ac393e016d914762bd190f7e4c125a
wilfried <wilfried.odorico@gmail.com>
2021-09-10 09:46:03 +0200

    ajout du formaulaire de connection

--- client/src/components/SigninForm.js
+++ client/src/components/SigninForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const SigninForm = () => {
   return (
-    <div>
-      Se connecter
+    <div className="container">
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate" />
+              <label for="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="password" type="password" className="validate" />
+              <label for="password">Password</label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SigninForm
+export default SigninForm;
