ce728d8a7d19a3ed50f0cc258edf3918c7602134
wilfried <wilfried.odorico@gmail.com>
2021-09-10 08:55:15 +0200

    creation de la navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <Routes />
-    </div>
+    </>
   );
 }
 
