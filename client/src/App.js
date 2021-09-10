6fa54b824a046a4b5d769ced6d7869234fe9a414
wilfried <wilfried.odorico@gmail.com>
2021-09-10 10:37:13 +0200

    ajout du burger menu navabar en vue mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 
 const App = () => {
+  useEffect(() => {
+    let sideNav = document.querySelector("#slide-out");
+    M.Sidenav.init(sideNav, {});
+  });
+
   return (
     <>
       <Navbar />
       <Routes />
     </>
   );
-}
+};
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,16 +9,31 @@ const Navbar = () => {
           <Link to="/" className="brand-logo">
             Groupomania
           </Link>
+          <Link to="#" data-target="slide-out" className="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <Link to="/signin">Se connecter</Link>
             </li>
             <li>
-              <Link to="/signup">S'inscrire</Link>
+              <Link to="/signup" className="btn waves-effect waves-light">
+                S'inscrire
+              </Link>
             </li>
           </ul>
         </div>
       </nav>
+      <ul class="sidenav" id="slide-out">
+        <li>
+          <Link to="/signin">Se connecter</Link>
+        </li>
+        <li>
+          <Link to="/signup" className="btn waves-effect waves-light">
+            S'inscrire
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
