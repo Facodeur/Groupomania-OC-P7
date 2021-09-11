3eafc62689885aeee19b9c096a35ea8162319fa3
wilfried <wilfried.odorico@gmail.com>
2021-09-11 18:00:29 +0200

    ajout methode recuperation user via jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
+import { UidContext } from "./context/UidContext";
+import axios from "axios";
 
 const App = () => {
-  
+  const [uid, setUid] = useState(null);
+
   useEffect(() => {
     M.AutoInit();
-  });
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/api/user/profile`,
+        withCredentials: true,
+      }).then((res) => {
+        console.log(res);
+        setUid(res.data);
+      }).catch(err => console.log(err))
+    };
+    fetchToken();
+  }, []);
 
   return (
-    <>
+    <UidContext.Provider value={uid}>
       <Navbar />
       <Routes />
-    </>
+    </UidContext.Provider>
   );
 };
 
