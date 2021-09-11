83724da721415f15eca753196317d803a1d4f133
wilfried <wilfried.odorico@gmail.com>
2021-09-11 17:59:55 +0200

    ajout methode login

--- client/src/components/SigninForm.js
+++ client/src/components/SigninForm.js
@@ -1,6 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const SigninForm = () => {
+  let history = useHistory();
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+        method: "post",
+        url: `${process.env.REACT_APP_API_URL}/api/user/login`,
+        withCredentials: true,
+        data: {
+          email,
+          password
+        }
+        
+      })
+      .then((res) => {
+        if (res.data.emailError || res.data.passwordError) {
+          setEmailError(res.data.emailError);
+          setPasswordError(res.data.passwordError);
+        } else {
+          console.log(res.data);
+          history.push("/");
+          setEmailError("");
+          setPasswordError("");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="container">
       <div className="row center-align">
@@ -11,19 +45,39 @@ const SigninForm = () => {
             style={{ height: 300, width: 300 }}
           />
         </div>
-        <form className="col l12 s12">
+        <form className="col l12 s12" onSubmit={handleSubmit}>
           <div className="row">
             <div className="input-field col s12">
-              <input id="email" type="email" className="validate" />
-              <label for="email">Email</label>
+              <label htmlFor="email">Email</label>
+              <input
+                className="validate"
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              {emailError && (
+                <p className="red-text left-align">{emailError}</p>
+              )}
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input id="password" type="password" className="validate" />
-              <label for="password">Password</label>
+              <label htmlFor="password">Password</label>
+              <input
+                className="validate"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {passwordError && (
+                <p className="red-text left-align">{passwordError}</p>
+              )}
             </div>
           </div>
+          <input type="submit" value="Se connecter" className="btn" />
         </form>
       </div>
     </div>
