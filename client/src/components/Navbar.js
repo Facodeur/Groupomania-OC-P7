cc05318355041ff17a5bd04e9720afecffb5e02a
wilfried <wilfried.odorico@gmail.com>
2021-09-13 15:43:50 +0200

    ajout logique d'affichage si user

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UidContext } from "../context/UidContext";
+import Logout from "./Logout";
 
 const Navbar = () => {
+  const uid = useContext(UidContext);
+
   return (
     <div>
       <nav>
@@ -12,16 +16,25 @@ const Navbar = () => {
           <Link to="#" data-target="slide-out" className="sidenav-trigger">
             <i className="material-icons">menu</i>
           </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/signin">Se connecter</Link>
-            </li>
-            <li>
-              <Link to="/signup" className="btn waves-effect waves-light">
-                S'inscrire
-              </Link>
-            </li>
-          </ul>
+          {uid ? (
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <h5>Bienvenue {uid.currentUser}</h5>
+              </li>
+              <Logout />
+            </ul>
+          ) : (
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/signin">Se connecter</Link>
+              </li>
+              <li>
+                <Link to="/signup" className="btn waves-effect waves-light">
+                  S'inscrire
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
       <ul className="sidenav" id="slide-out">
