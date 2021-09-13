31ed25af6823a6fda4bcf63e005da4d9e40ea321
wilfried <wilfried.odorico@gmail.com>
2021-09-13 20:57:22 +0200

    frontend creation du store redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'materialize-css/dist/css/materialize.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "materialize-css/dist/css/materialize.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+)
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
