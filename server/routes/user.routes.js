8a50baf79a87e48827d7aff297a46f061f006c93
wilfried <wilfried.odorico@gmail.com>
2021-09-14 10:49:29 +0200

    backend ajout de la route jwtid : recup id current user

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -10,10 +10,14 @@ router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
+//jwt récupération de l'id user
+router.get('/jwtid', authJwt, (req, res) => {
+  res.status(200).json(res.locals.user.id)
+});
+
 // Données utilisateur
 router.get("/profile", authJwt, userController.userInfo);
 router.put("/:id", authJwt, userController.userUpdate);
 router.delete("/:id", authJwt, userController.userDelete);
-
 
 module.exports = router;
