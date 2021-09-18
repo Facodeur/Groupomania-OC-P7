3dac3cca5df0e4d8dcc8615736d8496f3db0d4f9
wilfried <wilfried.odorico@gmail.com>
2021-09-18 12:53:49 +0200

    correction erreur methode: ajout du withCredentials : true

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -5,9 +5,11 @@ export const GET_USER = "GET_USER";
 export const getUser = () => {
   return async (dispatch) => {
     try {
-      const res = await axios
-        .get(`${process.env.REACT_APP_API_URL}/api/user/profile`);
-      console.log("user", res);
+      const res = await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/api/user/profile`,
+        withCredentials: true,
+      })
       dispatch({ type: GET_USER, payload: res.data });
     } catch (err) {
       return console.log(err);
