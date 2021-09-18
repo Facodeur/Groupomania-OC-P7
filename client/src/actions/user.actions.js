e74545b8b042a959bbaa9897e937a3434c55e72c
wilfried <wilfried.odorico@gmail.com>
2021-09-18 11:25:47 +0200

    mise en async await

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 export const GET_USER = "GET_USER";
 
-export const getUser = (uid) => {
-  return (dispatch) => {
-    return axios
-      .get(`${process.env.REACT_APP_API_URL}/api/user/${uid}`)
-      .then((res) => {
-        dispatch({ type: GET_USER, payload: res.data })
-      })
-      .catch((err) => console.log(err))
+export const getUser = () => {
+  return async (dispatch) => {
+    try {
+      const res = await axios
+        .get(`${process.env.REACT_APP_API_URL}/api/user/profile`);
+      console.log("user", res);
+      dispatch({ type: GET_USER, payload: res.data });
+    } catch (err) {
+      return console.log(err);
+    }
   }
 }
