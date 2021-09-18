fef4aa8e225f35e46731a4a7ec18fb93be21343d
wilfried <wilfried.odorico@gmail.com>
2021-09-18 12:51:52 +0200

    modif route useAuth

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from '../actions/user.actions';
 
 
 const useAuth = () => {
   const [authUser, setAuth] = useState(null);
   const [loadingUser, setLoadingUser] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
       await axios({
         method: "get",
-        url: `${process.env.REACT_APP_API_URL}/api/user/profile`,
+        url: `${process.env.REACT_APP_API_URL}/api/user/jwtid`,
         withCredentials: true,
       }).then((res) => {
         if(res)
@@ -18,10 +21,11 @@ const useAuth = () => {
       }).catch(err => console.log(err.message))
     };
     fetchToken();
-
+    if(authUser) dispatch(getUser());
+    
     return () => setLoadingUser(false);
 
-  }, [loadingUser])
+  }, [loadingUser, authUser, dispatch])
 
   return { authUser, setLoadingUser};
 }
