289172dcd51e55996955ba47cb57565a6ba71ba2
wilfried <wilfried.odorico@gmail.com>
2021-09-18 19:05:54 +0200

    creation sidebar

---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import Logout from "../Logout";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarWrapper,
+  SideBtnWrap,
+} from "./SidebarElements";
+
+const Sidebar = ({ isOpen, toggle }) => {
+  const { authUser } = useContext(UserContext);
+
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        {authUser ? (
+          <SidebarMenu>
+            <SidebarLink to="/profil" onClick={toggle}>
+              Profile
+            </SidebarLink>
+            <Logout onClick={toggle} />
+          </SidebarMenu>
+        ) : (
+          <>
+            <SidebarMenu>
+              <SidebarLink to="/signin" onClick={toggle}>
+                Se connecter
+              </SidebarLink>
+              <SideBtnWrap>
+                <SidebarRoute to="/signup" onClick={toggle}>
+                  S'enregistrer
+                </SidebarRoute>
+              </SideBtnWrap>
+            </SidebarMenu>
+          </>
+        )}
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
