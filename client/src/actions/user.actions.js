68e027a09b147d3df338853cb0cdd9a83b55d760
wilfried <wilfried.odorico@gmail.com>
2021-09-20 13:51:09 +0200

    refacto

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -3,32 +3,29 @@ import axios from "axios";
 export const GET_USER = "GET_USER";
 export const DELETE_USER = "DELETE_USER";
 
-export const getUser = () => {
-  return async (dispatch) => {
-    try {
-      const res = await axios({
-        method: "get",
-        url: `${process.env.REACT_APP_API_URL}/api/user/profile`,
-        withCredentials: true,
-      });
-      dispatch({ type: GET_USER, payload: res.data });
-    } catch (err) {
-      return console.log(err);
-    }
-  };
+export const getUser = () => async (dispatch) => {
+  try {
+    const res = await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/api/user/profile`,
+      withCredentials: true,
+    });
+    dispatch({ type: GET_USER, payload: res.data });
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
-export const deleteUser = (uid) => {
-  return async (dispatch) => {
-    try {
-      const res = await axios({
-        method: "delete",
-        url: `${process.env.REACT_APP_API_URL}/api/user/${uid}`,
-        withCredentials: true,
-      });
-      dispatch({ type: DELETE_USER, payload: res.data });
-    } catch (err) {
-      return console.log(err);
-    }
-  };
+
+export const deleteUser = (uid) => async (dispatch) => {
+  try {
+    const res = await axios({
+      method: "delete",
+      url: `${process.env.REACT_APP_API_URL}/api/user/${uid}`,
+      withCredentials: true,
+    });
+    dispatch({ type: DELETE_USER, payload: res.data });
+  } catch (err) {
+    return console.log(err);
+  }
 };
