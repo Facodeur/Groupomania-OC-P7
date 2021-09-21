af327a57decdffdc0903cc7177a42df751456981
wilfried <wilfried.odorico@gmail.com>
2021-09-21 15:18:49 +0200

    construction du cardPost + stylisation

--- client/src/components/CardPost/CardPostElements.js
+++ client/src/components/CardPost/CardPostElements.js
@@ -1,33 +1,76 @@
 import styled from "styled-components";
 import { FaUserCircle } from "react-icons/fa";
+import { BiCommentDetail } from "react-icons/bi";
 
-export const CardContainer = styled.div`
+export const CardContainer = styled.li`
   display: flex;
   flex-direction: column;
   border: 2px solid #15cdfc;
-  border-radius: 10px;
-  padding: 20px;
+  border-radius: 5px;
+  padding: 15px;
   margin: 10px;
-  width: 500px;
+  width: auto;
 `
 
-export const Header = styled.div`
+export const CardHeader = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 10px;
+  min-height: 35px;
 `
 
 export const Icon = styled(FaUserCircle)`
-  width: 30px;
-  height: 30px;
+  width: 20px;
+  height: 20px;
   color: #000;
-  margin-right: 10px;
+  margin-right: 5px;
 `
 
-export const Username = styled.h3`
+export const CardUsername = styled.h3`
+  display: flex;
+  align-items: center;
   font-size: 1rem;
+`
+
+export const CardText = styled.p`
+  font-size: 1rem;
+  margin: 5px;
+`
+
+export const CardPicture = styled.img`
+  margin: 2px;
+  border-radius: 5px;
+`
+
+export const CardFooter = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 10px;
+  color: #57606f;
+  padding-top: 10px;
+`
+
+export const IconComment = styled(BiCommentDetail)`
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+
+  &:hover {
+    cursor: pointer;
+    transition: 0.2s all ease-in-out;
+    transform: scale(1.25);
+  }
 `
 
 export const DatePosted = styled.p`
-  font-size: 1rem;
+  font-size: 0.8rem;
   color: #7f8c8d;
+`
+
+export const LoadingIcon = styled.div`
+display: flex;
+justify-content: center;
+font-size: 2rem;
+color: #7f8c8d;
 `
--- client/src/components/CardPost/index.js
+++ client/src/components/CardPost/index.js
@@ -1,24 +1,53 @@
-import React from 'react'
-import { dateParser } from '../../utils/date-parser'
-import { CardContainer, DatePosted, Header, Icon, Username } from './CardPostElements'
+import React, { useEffect, useState } from "react";
+import { dateParser } from "../../utils/date-parser";
+import { FaSpinner } from "react-icons/fa";
+import {
+  CardContainer,
+  DatePosted,
+  CardHeader,
+  Icon,
+  CardUsername,
+  LoadingIcon,
+  CardText,
+  CardPicture,
+  CardFooter,
+  IconComment,
+} from "./CardPostElements";
 
 const CardPost = ({ post }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(post)
 
+  useEffect(() => {
+    post && setIsLoading(false)
+  }, [post]);
 
   return (
     <CardContainer>
-      <Header>
-          <Username>
-          <Icon/>
-          {post.User.username}
-        </Username>
-        <DatePosted>
-          {dateParser(post.createdAt)}
-        </DatePosted>
-      </Header>
-      card post
+      {isLoading ? (
+        <LoadingIcon>
+          <FaSpinner className="fa-spin" />
+        </LoadingIcon>
+      ) : (
+        <>
+          <CardHeader>
+            <CardUsername>
+              <Icon />
+              {post.User.username}
+            </CardUsername>
+            <DatePosted>{dateParser(post.createdAt)}</DatePosted>
+          </CardHeader>
+          <CardText>
+            {post.content}
+          </CardText>
+          {post.picture && <CardPicture src={post.picture} alt="post" />} 
+          <CardFooter>
+            <IconComment />{" "} {post.Comments.length}
+          </CardFooter>
+        </>
+      )}
     </CardContainer>
-  )
-}
+  );
+};
 
-export default CardPost
+export default CardPost;
