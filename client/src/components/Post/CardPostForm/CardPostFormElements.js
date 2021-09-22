113772b2063d7ca84f7fbae83212958b56cdd800
wilfried <wilfried.odorico@gmail.com>
2021-09-22 20:10:15 +0200

    creation CardPostForm

---
+++ client/src/components/Post/CardPostForm/CardPostFormElements.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+import { FaUserCircle } from "react-icons/fa";
+
+export const CardContenair = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #000;
+  margin: 20px;
+  border-radius: 5px;
+  min-width: 300px;
+  max-width: 600px;
+`
+
+export const CardRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 20px;
+`
+
+export const CardTextarea = styled.textarea`
+  border: none;
+  border-radius: 5px;
+  overflow: auto;
+  outline: none;
+  resize: none;
+  width: 100%;
+  height: 40px;
+  padding: 7px;
+  font-size: 1rem;
+`
+
+export const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  color: #fff;
+`
+
+export const BtnPictureWrapper = styled.div`
+  margin-left: 20px;
+`
+
+export const BtnIcon = styled.button`
+  background: #000;
+  color: #fff;
+  border: none;
+  font-size: 1.5rem;
+
+  &:hover {
+    cursor: pointer;
+    transition: 0.2s;
+    color: #15cdfc;
+  }
+`
+
+export const BtnSend = styled.button`
+  background: #15cdfc;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+  margin-bottom: 15px;
+  margin-right: 20px;
+  font-size: 0.9rem;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+    transition: 0.2s;
+    opacity: 0.9;
+  }
+`
+
+export const LoadingIcon = styled.div`
+display: flex;
+justify-content: center;
+font-size: 2rem;
+color: #7f8c8d;
+`
+
+export const ProfilIcon = styled(FaUserCircle)`
+  width: 30px;
+  height: 30px;
+  color: ${({color}) => color ? color : "#fff"};
+  margin-right: 15px;
+`
+
+export const MessageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #15cdfc;
+  background: #fff;
+  margin: 20px;
+  padding: 10px;
+  border-radius: 5px;
+`
+
+export const MessageRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px;
+`
+
+export const MessageContent = styled.p`
+  margin: 10px;
+`
+
+export const MessagePicture = styled.img`
+  margin: 10px;
+`
---
+++ client/src/components/Post/CardPostForm/index.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useRef, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { AiOutlinePicture } from "react-icons/ai";
+import { timestampParser } from "../../../utils/date-parser";
+import { addPost, getPosts } from "../../../actions/post.action";
+import {
+  CardContenair,
+  CardTextarea,
+  CardRow,
+  LoadingIcon,
+  ProfilIcon,
+  CardFooter,
+  BtnIcon,
+  BtnPictureWrapper,
+  BtnSend,
+  MessageWrapper,
+} from "./CardPostFormElements";
+import {
+  CardHeader,
+  CardPicture,
+  CardText,
+  CardUsername,
+  DatePosted,
+  Icon,
+} from "../CardPost/CardPostElements";
+
+const CardPostForm = () => {
+  const fileInputRef = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState("");
+  const [picture, setPicture] = useState(null);
+  const [file, setFile] = useState();
+  const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+  console.log("form", userData);
+
+  const handlePost = () => {
+    if (message || picture) {
+      const data = new FormData();
+      data.append("content", message);
+      if (file) {
+        data.append("image", file);
+      }
+      dispatch(addPost(data));
+      dispatch(getPosts());
+      cancelPost();
+    } else {
+      alert("Veuillez entrer un message");
+    }
+  };
+
+  const handlePicture = (e) => {
+    setPicture(URL.createObjectURL(e.target.files[0]));
+    setFile(e.target.files[0]);
+  };
+
+  const cancelPost = () => {
+    setMessage("");
+    setPicture("");
+    setFile("");
+  };
+
+  useEffect(() => {
+    if (userData) {
+      setIsLoading(false);
+    }
+  }, [userData]);
+
+  return (
+    <CardContenair>
+      {isLoading ? (
+        <LoadingIcon>
+          <FaSpinner className="fa-spin" />
+        </LoadingIcon>
+      ) : (
+        <>
+          <CardRow>
+            <ProfilIcon />
+            <CardTextarea
+              name="message"
+              placeholder={`Quoi de neuf ${userData.username} ?`}
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+          </CardRow>
+          {message || picture ? (
+            <>
+              <MessageWrapper>
+                <CardHeader>
+                  <CardUsername>
+                    <Icon />
+                    {userData.username}
+                  </CardUsername>
+                  <DatePosted>{timestampParser(Date.now())}</DatePosted>
+                </CardHeader>
+                <CardText>{message}</CardText>
+                <CardPicture src={picture} />
+              </MessageWrapper>
+            </>
+          ) : null}
+          <CardFooter>
+            <BtnPictureWrapper>
+              <BtnIcon onClick={() => fileInputRef.current.click()}>
+                <AiOutlinePicture />
+              </BtnIcon>
+              <input
+                ref={fileInputRef}
+                type="file"
+                name="file"
+                onChange={(e) => handlePicture(e)}
+                accept=".jpg, .jpeg, .png, .gif"
+                hidden
+              />
+            </BtnPictureWrapper>
+            {message || picture ? (
+              <button onClick={cancelPost}>Annuler message</button>
+            ) : null}
+            <BtnSend onClick={handlePost}>Envoyer</BtnSend>
+          </CardFooter>
+        </>
+      )}
+    </CardContenair>
+  );
+};
+
+export default CardPostForm;
