f7f0ca57f4d0a62c61ca08edd187d62d24dacc6e
wilfried <wilfried.odorico@gmail.com>
2021-09-22 20:09:03 +0200

    ajout reglage global

--- client/src/theme/GlobalStyle.js
+++ client/src/theme/GlobalStyle.js
@@ -2,10 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
+  ::selection {
+    background: #61dafb;
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     font-family: 'Lato', sans-serif;
   }
-`
+
+  body {
+    font-size: 16px;
+    transition: 600ms;
+  }
+`;
