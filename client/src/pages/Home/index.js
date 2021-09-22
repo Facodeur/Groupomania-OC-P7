ffb66c4c0c47b2d894ce60bba388d6bb9051512d
wilfried <wilfried.odorico@gmail.com>
2021-09-22 15:32:36 +0200

    condition affichage postForm

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { HomeContainer, HomePostList } from "./HomeElements";
 import CardPostList from "../../components/CardPostList";
+import { UserContext } from "../../context/UserContext";
+import CardPostForm from "../../components/CardPostForm";
 
 const Home = () => {
+  const  { authUser }  = useContext(UserContext);
+  console.log("home", authUser)
 
   return (
     <HomeContainer>
-      {}
+      {authUser && <CardPostForm /> }
       <HomePostList>
         <CardPostList />
       </HomePostList>
