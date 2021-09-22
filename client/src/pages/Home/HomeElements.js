cdc92509acbbd234aab92935be1bfcb0342628c8
wilfried <wilfried.odorico@gmail.com>
2021-09-22 15:32:52 +0200

    ajout flex column

--- client/src/pages/Home/HomeElements.js
+++ client/src/pages/Home/HomeElements.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `
 
 export const HomePostList = styled.ul`
