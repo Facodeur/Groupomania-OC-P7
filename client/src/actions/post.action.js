5dfb470a5b0c937d3b7a03c7ad2b9284f909a54f
wilfried <wilfried.odorico@gmail.com>
2021-09-22 20:10:51 +0200

    ajout de action addPost

--- client/src/actions/post.action.js
+++ client/src/actions/post.action.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POSTS = "ADD_POSTS";
 
 export const getPosts = (num) => async (dispatch) => {
   try {
@@ -15,3 +16,16 @@ export const getPosts = (num) => async (dispatch) => {
     return console.log(err);
   }
 };
+
+export const addPost = (data) => async (dispatch) => {
+  try {
+    await axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API_URL}/api/post`,
+      withCredentials: true,
+      data: data,
+    });
+  } catch (err) {
+    return console.log(err);
+  }
+}
