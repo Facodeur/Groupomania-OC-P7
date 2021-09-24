e7b4d6f9269d400ac527b2a47d72cb10a924cd06
wilfried <wilfried.odorico@gmail.com>
2021-09-24 11:40:36 +0200

    ajout methode update et delete àaction et reducer

--- client/src/actions/post.action.js
+++ client/src/actions/post.action.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const GET_POSTS = "GET_POSTS";
 export const ADD_POSTS = "ADD_POSTS";
+export const UPDATE_POST = "UPDATE_POST";
+export const DELETE_POST = "DELETE_POST";
 
 export const getPosts = (num) => async (dispatch) => {
   try {
@@ -28,4 +30,33 @@ export const addPost = (data) => async (dispatch) => {
   } catch (err) {
     return console.log(err);
   }
-}
+};
+
+export const updatePost = (postId, content) => {
+  return (dispatch) => {
+    return axios({
+      method: "PUT",
+      url: `${process.env.REACT_APP_API_URL}/api/post/${postId}`,
+      withCredentials: true,
+      data: { content },
+    })
+      .then((res) => {
+        dispatch({ type: UPDATE_POST, payload: { content, postId } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const deletePost = (postId) => {
+  return (dispatch) => {
+    return axios({
+      method: "DELETE",
+      url: `${process.env.REACT_APP_API_URL}/api/post/${postId}`,
+      withCredentials: true,
+    })
+      .then((res) => {
+        dispatch({ type: DELETE_POST, payload: { postId } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
