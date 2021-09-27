113bd4dc642605fbc5ab29ad1ff82551e740305d
wilfried <wilfried.odorico@gmail.com>
2021-09-27 10:32:32 +0200

    creation compo et route privé admin

---
+++ client/src/Routes/PrivateRouteAdmin.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+const PrivateRouteAdmin = ({component: Component, ...rest}) => {
+  const { authUser } = useContext(UserContext);
+  
+  return (
+
+      // Show the component only when the user is logged in
+      // Otherwise, redirect the user to /signin page
+      <Route {...rest} render={props => (
+        authUser.isAdmin === 1 ?
+              <Component {...props} />
+          : <Redirect to="/signin" />
+      )} />
+  );
+};
+
+export default PrivateRouteAdmin;
--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -5,12 +5,15 @@ import SignupForm from "../components/LogForm/SignupForm";
 import Profil from '../pages/Profil';
 import Home from "../pages/Home";
 import PrivateRoute from './PrivateRoute';
+import PrivateRouteAdmin from './PrivateRouteAdmin';
+import BoardAdmin from '../pages/BoardAdmin/UserManagement';
 
 const index = () => {
   return (
       <Switch>
         <Route exact path="/" component={Home} />
         <PrivateRoute exact path="/profil" component={Profil} />
+        <PrivateRouteAdmin exact path="/board-admin" component={BoardAdmin} />
         <Route exact path="/signin" component={SigninForm} />
         <Route exact path="/signup" component={SignupForm} />
         <Redirect to="/" />
